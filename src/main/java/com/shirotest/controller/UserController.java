815bf1060b842d46e95ee3143f762a68b5587eaf
热爱与向往 <59462015+lgs2git@users.noreply.github.com>
2021-03-26 19:07:19 +0800

    Update UserController.java

--- src/main/java/com/shirotest/controller/UserController.java
+++ src/main/java/com/shirotest/controller/UserController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+* aaa
+*/
 @RestController
 public class UserController {
 
